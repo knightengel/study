e57443243ee20153d1bcbdf86a6c8903d12266b9
knightengel <knightoftheniche@gmail.com>
2026-02-27 11:25:39 +0300

    Add pyright

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   pkgs = import <nixpkgs> {};
   pythonEnv = pkgs.python3.withPackages (ps: with ps; [
     pyqt6
-    # Если нужны еще пакеты, добавляйте их сюда, например: requests, numpy
+    pyright
   ]);
 in
 pkgs.mkShell {
